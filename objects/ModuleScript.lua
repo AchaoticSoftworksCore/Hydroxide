c6e8e6e304c33e5e5c00a91dd0ba0ba445deb0d9
fusion <evandusud1234@gmail.com>
2025-09-17 02:00:42 +0200

    Update ModuleScript.lua

--- objects/ModuleScript.lua
+++ objects/ModuleScript.lua
@@ -2,12 +2,6 @@ local ModuleScript = {}
 
 function ModuleScript.new(instance)
     local moduleScript = {}
-    local closure = getScriptClosure(instance)
-
-    moduleScript.Instance = instance
-    -- moduleScript.Constants = getConstants(closure)
-    moduleScript.Protos = getProtos(closure)
-    --moduleScript.ReturnValue = require(instance) // causes detection
 
     return moduleScript
 end
